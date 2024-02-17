05257a5340d0bfd1e7e9737ef8895f31e5b929b6
Pavan Sai <pavansaidevops7@gmail.com>
2024-02-17 19:46:44 +0530

    terraform elasticache

--- vars.tf
+++ vars.tf
@@ -6,9 +6,7 @@ variable "sg_ingress_cidr" {}
 variable "elasticache_type" {}
 variable "family" {}
 variable "port" {}
-variable "cluster_id" {}
 variable "engine" {}
 variable "node_type" {}
 variable "num_cache_nodes" {}
-variable "parameter_group_name" {}
 variable "engine_version" {}
